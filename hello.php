56c30db0a944b73afa80350efe1dccdaf281e231
EliRalli <elikrallison@gmail.com>
2022-12-07 10:04:58 -0700

    playing around with php and while loops

--- hello.php
+++ hello.php
@@ -6,27 +6,20 @@
 <?php
 
 
-    //echo "Hello World how fast does this update?";
-   // $str = 'Hello & Welcome to mylocalserverwebpage';
-    //$num = 8.8;
+    $numbers = [56, 65, 26, 86, 66, 34, 78, 74, 67, 18, 34, 73, 45, 67, 75, 10, 60, 80, 74, 16, 86, 34, 12, 23, 42, 72, 36, 3, 73, 9, 92, 81, 94, 54, 97, 74, 45, 55, 70, 94, 96, 81, 86, 86, 84, 4, 32, 8, 96, 86, 87, 18, 84, 87, 59, 48, 32, 90, 17, 22, 82, 79, 66, 28, 17, 14, 80, 83, 66, 36, 21, 89, 68, 2, 51, 65, 20, 87, 48, 5, 1, 16, 60, 53, 84, 90, 16, 2, 37, 73, 57, 70, 57, 69, 68, 1, 24, 40, 72, 97];
+
     $brands = array("HP","Lenovo","Asus","dell");
 
-    echo var_dump($brands); echo ".\n";
-    //echo "<h1>" . $str . $num . "<h1>";
 
-    class Car {
-        public function __construct($brand, $year) {
-            $this->brand = $brand;
-            $this->year = $year;
-        }
-    
-        public function print_details() {
-            echo "This car is a " . $this->year . " " . $this->brand . ".\n";
-        }
+    $index= 0;
+    while( $index < count($numbers))
+    {
+        $number=$numbers[$index]; //using a while loop to print all odd numbers in array
+        ++$index;
+        if( $number % 2 == 0)
+            continue;
+        echo "$number\n";    
     }
-    
-    $car = new Car("Toyota", 2006);
-    $car->print_details();
 ?>
 
 </body>
